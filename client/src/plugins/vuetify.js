49e0d5201c821275f0e15632ec5d29641fb391d8
DuyguKeskek <duygukeskek@hotmail.com>
2018-09-13 18:50:06 +0300

    created basic frontend structure with Vue

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+    theme: 
+    {
+      primary: "#223843",
+      secondary: "#D77A61",
+      accent: "#D77A61",
+      error: "#f44336",
+      warning: "#ffeb3b",
+      info: "#2196f3",
+      success: "#4caf50"
+    }
+})
--- server.js
+++ server.js
@@ -4,15 +4,18 @@ mongoose.Promise = global.Promise;
 const fs = require('fs')
 const path = require('path')
 
+// Import typedefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql')
 const typeDefs = fs.readFileSync(filePath, 'utf-8')
 const resolvers = require('./resolvers')
 
+// Import Mongoose models
 const User = require('./models/User')
 const Post = require('./models/Post')
 
 MONGO_URI= '_URI_'
 
+// Connect to MLab database
 mongoose
     .connect(
         MONGO_URI,
@@ -21,6 +24,7 @@ mongoose
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err))
 
+// Create Apollo/GraphQL server using typedefs, resolvers, and context object
 const server = new ApolloServer({
     typeDefs,
     resolvers,
